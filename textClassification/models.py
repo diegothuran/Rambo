59da65dcbe36d617079210fe5aa4a4eff4fcfda3
diegothuran <diego.thuran@gmail.com>
2017-06-05 15:40:00 -0300

    Atualização de Views no admin

--- textClassification/models.py
+++ textClassification/models.py
@@ -4,11 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Comment(models.Model):
-    comment = models.TextField()
-    is_product = models.BooleanField(blank=True)
-    is_store = models.BooleanField(blank=True)
-    is_correct = models.BooleanField(blank=True, default=True)
-    is_to_update = models.BooleanField(blank=True, default=False)
+    comment = models.TextField(verbose_name="Review")
+    is_product = models.BooleanField(blank=True, verbose_name="É de produto?")
+    is_store = models.BooleanField(blank=True, verbose_name="É de Loja?")
+    is_correct = models.BooleanField(blank=True, default=True, verbose_name="Está correto?")
+    is_to_update = models.BooleanField(blank=True, default=False, verbose_name="Utilizar na atualização?")
 
-    def __str__(self):
-        return self.comment[:40]
+    #def __str__(self):
+    #    return u'Model: %s' % self.comment
+
+    def __unicode__(self):
+        return u'Model: %s' % self.comment
